5c472d086fc9d5f3da7f1477cb8b66e2b9486181
Elena <eshipkova@iMac-Dmitry.local>
2022-05-22 22:04:12 +0300

    Minimal edits

--- posts/articles/admin.py
+++ posts/articles/admin.py
@@ -5,15 +5,16 @@ from .models import Article, Author
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'birth_date')
-    search_fields = ('name',)
+    search_fields = ('name__startswith',)
     list_filter = ('name',)
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'text', 'pub_date')
-    search_fields = ('pub_date',)
-    list_filter = ('title',)
+    search_fields = ('title__icontains',)
+    list_filter = ('pub_date', 'authors__name')
     filter_horizontal = ('authors',)
+    empty_value_display = '-пусто-'
 
 
 admin.site.register(Author, AuthorAdmin)
